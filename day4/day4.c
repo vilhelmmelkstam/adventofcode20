68cea9e7dff194c64e9b66ae65733d469a1320a7
Vilhelm Melkstam <vilhelm.melkstam@gmail.com>
2020-12-09 23:24:53 +0100

    Add solution for day 4

---
+++ day4/day4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  int validPassports = 0;
+
+  // Keep track of which fields have been found
+  _Bool byr, iyr, eyr, hgt, hcl, ecl, pid;
+
+  char field[100];
+  while (fgets(field, 100, stdin) != NULL) // Read line
+  {
+    if (field[0] != '\n') // If not empty line
+    {
+      byr = byr || strstr(field, "byr:") != NULL; // Set true if field found
+      iyr = iyr || strstr(field, "iyr:") != NULL;
+      eyr = eyr || strstr(field, "eyr:") != NULL;
+      hgt = hgt || strstr(field, "hgt:") != NULL;
+      hcl = hcl || strstr(field, "hcl:") != NULL;
+      ecl = ecl || strstr(field, "ecl:") != NULL;
+      pid = pid || strstr(field, "pid:") != NULL;
+    }
+    else // If empty line
+    {
+      if (byr && iyr && eyr && hgt && hcl && ecl && pid) // Add if all fields were found
+        validPassports++;
+
+      // Reset field flags
+      byr = 0;
+      iyr = 0;
+      eyr = 0;
+      hgt = 0;
+      hcl = 0;
+      ecl = 0;
+      pid = 0;
+    }
+  }
+
+  printf("%d", validPassports);
+}
